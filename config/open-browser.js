9841c2319de4b809ff3486621d198f8bb02d48f1
Iristyle <Iristyle@github>
2016-10-16 23:02:53 -0700

    Reconfigure PE install for local git hosting
     - Ensure that Windows 10 box starts first since it's hosting git
     - But that means that the PE master at davis-master.vm won't be up
       yet, so remove the call to config-client-tools as that will now
       have to be done after PE is completely provisioned

--- config/open-browser.js
+++ config/open-browser.js
@@ -4,6 +4,7 @@ var objIE = new ActiveXObject("InternetExplorer.Application");
 objIE.Visible = true;
 objIE.Navigate2("https://davis-master.vm");
 objIE.Navigate2("http://win2012-choco", navOpenInBackgroundTab);
+objIE.Navigate2("http://localhost/puppet/controlrepo", navOpenInBackgroundTab);
 objIE.Navigate2("http://loadbalancer.vm:9000", navOpenInBackgroundTab);
 objIE.Navigate2("http://loadbalancer.vm", navOpenInBackgroundTab);
 objIE.Navigate2("https://github.com/puppetlabs/puppetlabs-app_modeling", navOpenInBackgroundTab);
@@ -11,4 +12,3 @@ objIE.Navigate2("http://win2012-web-green-1", navOpenInBackgroundTab);
 objIE.Navigate2("http://win2012-web-green-2", navOpenInBackgroundTab);
 objIE.Navigate2("http://win2012-web-blue-1", navOpenInBackgroundTab);
 objIE.Navigate2("http://win2012-web-blue-2", navOpenInBackgroundTab);
-
