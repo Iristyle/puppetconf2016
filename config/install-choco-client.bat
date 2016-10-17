526a63ff89163a1d180419ffb799659387c4eda8
Iristyle <Iristyle@github>
2016-10-17 04:55:24 -0700

    Make choco install script more idempotent

--- config/install-choco-client.bat
+++ config/install-choco-client.bat
@@ -1,4 +1,10 @@
+choco --version > nul
+
+if %ERRORLEVEL% EQU 0 goto INSTALLED
+
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+:INSTALLED
 
 choco source add -n=%1 -s"http://%2/chocolatey/Packages"
 choco apikey -s"http://%2/chocolatey/Packages" -k="%3"
